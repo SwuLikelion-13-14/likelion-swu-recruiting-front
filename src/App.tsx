7300a1816fe6e07a505ee8c15977a2a443994198
5cool9 <pumpkim12345@naver.com>
2026-02-03 14:15:11 +0900

    Init: 초기 환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-3xl font-bold">
+      Tailwind 적용 
+    </div>
+  )
+}
+
+export default App
