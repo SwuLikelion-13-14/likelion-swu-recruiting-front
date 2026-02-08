28ce70d65576ce03f4998733812da8a7e5df4730
jeongkyueun <kyueun@swu.ac.kr>
2026-02-08 20:31:54 +0900

    feat: 1_홈페이지 구현 완료 및 레이아웃 css 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import FrontPage from '@/pages/apply/FrontPage'
+import HomePage from './pages/home/HomePage';
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/front" element={<FrontPage />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   )
 }
