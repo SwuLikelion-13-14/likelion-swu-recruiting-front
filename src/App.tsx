ada3308b14ccbda4cb4e3f01fe86ca43846a988c
Park jihyun <thotemily@naver.com>
2026-02-14 01:20:23 +0900

    Feat : 모바일 환경 시 모달 추가

--- src/App.tsx
+++ src/App.tsx
@@ -12,39 +12,45 @@ import ApplyPage from "@/pages/apply/ApplyPage";
 import RecruitTrackPage from "./pages/apply/RecruitTrackPage";
 import FrontPage from "@/pages/apply/FrontPage";
 import DesignPage from "./pages/apply/DesignPage";
-import BackPage from "./pages/apply/BackPage";
+import BackPage from "@/pages/apply/BackPage";
 
 import AdminLayout from "@/pages/admin/AdminLayout";
 import AdminApplicationsPage from "@/pages/admin/AdminApplicationsPage";
 import AdminApplicationDetailPage from "@/pages/admin/AdminApplicationDetailPage";
 import AdminSchedulePage from "@/pages/admin/AdminSchedulePage";
 
+import MobileGuardModal from "@/components/MobileGuardModal";
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/annual-plan" element={<AnnualPlanPage />} />
-      <Route path="/project-list" element={<ProjectListPage />} />
-      <Route path="/faq" element={<FaqPage />} />
-      <Route path="/apply" element={<ApplyPage />} />
-      <Route path="/activity-content" element={<ActivityContentPage />} />
-      <Route path="/leaders" element={<LeadersPage />} />
-      <Route path="/recruit" element={<RecruitPage />} />
-      <Route path="/recruit-track" element={<RecruitTrackPage />} />
-      <Route path="/front" element={<FrontPage />} />
-      <Route path="/design" element={<DesignPage />} />
-      <Route path="/back" element={<BackPage />} />
+    <>
+      <MobileGuardModal breakpoint={375} />
 
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<Navigate to="applications" replace />} />
-        <Route path="applications" element={<AdminApplicationsPage />} />
-        <Route path="schedule" element={<AdminSchedulePage />} />
-        <Route
-          path="applications/detail/:code"
-          element={<AdminApplicationDetailPage />}
-        />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/annual-plan" element={<AnnualPlanPage />} />
+        <Route path="/project-list" element={<ProjectListPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="/apply" element={<ApplyPage />} />
+        <Route path="/activity-content" element={<ActivityContentPage />} />
+        <Route path="/leaders" element={<LeadersPage />} />
+        <Route path="/recruit" element={<RecruitPage />} />
+        <Route path="/recruit-track" element={<RecruitTrackPage />} />
+        <Route path="/front" element={<FrontPage />} />
+        <Route path="/design" element={<DesignPage />} />
+        <Route path="/back" element={<BackPage />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Navigate to="applications" replace />} />
+          <Route path="applications" element={<AdminApplicationsPage />} />
+          <Route path="schedule" element={<AdminSchedulePage />} />
+          <Route
+            path="applications/detail/:code"
+            element={<AdminApplicationDetailPage />}
+          />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
