f8ae6b43e0d14fc86748e130d05600b93fea59f1
5cool9 <pumpkim12345@naver.com>
2026-02-16 00:51:33 +0900

    Feat: 서류모집 마감 화면 날짜 반영 테스트

--- src/pages/apply/ApplyPage.tsx
+++ src/pages/apply/ApplyPage.tsx
@@ -4,11 +4,31 @@ import Layout from '../../components/Layout/Layout';
 import styles from './ApplyPage.module.css';
 import ApplyBox from '@/components/ApplyBox/ApplyBox'
 import chevronRight from '@/assets/icon/chevron_right_small.svg';
+import { useEffect } from 'react';
 
 
 const ApplyPage = () => {
 
-const navigate = useNavigate();
+    const navigate = useNavigate();
+    useEffect(() => {
+        // 목표 시간: 2월 16일 오전 12시 50분
+        const targetDate = new Date('2026-02-16T00:55:00');
+
+        const now = new Date();
+        const delay = targetDate.getTime() - now.getTime(); // ms 단위
+
+        if (delay > 0) {
+            const timer = setTimeout(() => {
+                navigate('/faq'); // 목표 시간 이후 페이지 이동
+            }, delay);
+
+            return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+        } else {
+            // 이미 목표 시간이 지난 경우 바로 이동
+            navigate('/faq');
+        }
+    }, [navigate]);
+
     return (
         <div
             className={styles.container}
@@ -21,7 +41,7 @@ const navigate = useNavigate();
                     {/* 중앙 사각형 박스 */}
                     <ApplyBox />
                     <button className={styles.bottomButton}
-                    onClick={() => navigate('/recruit')} >
+                        onClick={() => navigate('/recruit')} >
                         <div className={styles.bottomButtonBg} />
                         <span>14기 아기사자 모집 정보 보기</span>
                         <img src={chevronRight} alt="arrow" className={styles.chevronRight} />
